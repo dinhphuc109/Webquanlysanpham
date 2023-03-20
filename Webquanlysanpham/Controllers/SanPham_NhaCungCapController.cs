8ee4526698106c614a93e50ed2df40580a1a63ef
dinhphuc109 <dinhphuc111@gmail.com>
2023-03-20 14:38:22 +0700

    add edit delete Sp_Ncc

--- Webquanlysanpham/Controllers/SanPham_NhaCungCapController.cs
+++ Webquanlysanpham/Controllers/SanPham_NhaCungCapController.cs
@@ -43,5 +43,72 @@ namespace Webquanlysanpham.Controllers
 
             return View("Index");
         }
+
+        [HttpGet]
+        public IActionResult Edit(long? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var SanPham_NhaCungCapFromDb = _db.sanPham_NhaCungCaps.Find(id);
+            if (SanPham_NhaCungCapFromDb == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Listnhacungcap = new SelectList(_db.nhaCungCaps.ToList(), "ID", "TenNCC");
+            ViewBag.Listsanpham = new SelectList(_db.sanPhams.ToList(), "ID", "TenSP");
+            return View(SanPham_NhaCungCapFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(SanPham_NhaCungCap obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.sanPham_NhaCungCaps.Update(obj);
+                _db.SaveChanges();
+                TempData["Thanh cong"] = "Sua Thanh Cong";
+                return RedirectToAction("Index");
+            }
+            ViewBag.Listnhacungcap = new SelectList(_db.nhaCungCaps.ToList(), "ID", "TenNCC");
+            ViewBag.Listsanpham = new SelectList(_db.sanPhams.ToList(), "ID", "TenSP");
+            return View(obj);
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(long? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var SanPham_NhaCungCapFromDb = _db.sanPham_NhaCungCaps.Find(id);
+            if (SanPham_NhaCungCapFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(SanPham_NhaCungCapFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(long? id)
+        {
+            var obj = _db.sanPham_NhaCungCaps.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.sanPham_NhaCungCaps.Remove(obj);
+            _db.SaveChanges();
+            TempData["Thanh cong"] = "Xoa Thanh Cong";
+            return RedirectToAction("Index");
+
+
+        }
     }
 }
